d9726cff6ef7160bd95a8103ab88ba4cd6ad50a9
Vedran Smid <vedransmid@yeti-it.hr>
2020-05-08 09:57:40 +0200

    run_native-image_agent.sh now uses $JAVA_HOME env. variable.

--- run_native-image_agent.sh
+++ run_native-image_agent.sh
@@ -1,1 +1,1 @@
-java -agentlib:native-image-agent=config-merge-dir=META-INF/native-image -cp target/classes hr.yeti.notebook.Nb
+$JAVA_HOME/bin/java -agentlib:native-image-agent=config-merge-dir=META-INF/native-image -cp target/classes hr.yeti.notebook.Nb
